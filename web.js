83c9dfe2b2e6c5006d7b813fb4cebd609d9ae879
Michal Cichon <michal.cichon@aol.com>
2013-07-10 21:51:51 +0000

    HW3 Get content from file (3)

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync("index.html");
-  response.send(content);
+    var content = fs.readFileSync("index.html");
+    response.send(content.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
